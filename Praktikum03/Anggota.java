e382caca46f54473e1e25a58e925ff39ee25e271
RAFFMONNN <rafaalrabbani08@gmail.com>
2026-09-07 18:37:53 +0700

    MODIFIKASI ANGSURAN

--- Praktikum03/Anggota.java
+++ Praktikum03/Anggota.java
@@ -17,22 +17,23 @@ public class Anggota {
     public String getNama() {
         return nama;
     }
+
     public void setNama(String nama) {
         this.nama = nama;
     }
 
-
     public int getNomorKTP() {
         return nomorKTP;
     }
+
     public void setNomorKTP(int nomorKTP) {
         this.nomorKTP = nomorKTP;
     }
 
-
     public int getLimitPinjaman() {
         return limitPinjaman;
     }
+
     public void setLimitPinjaman(int limitPinjaman) {
         if (limitPinjaman < 0) {
             System.out.println("Limit pinjaman tidak boleh bernilai negatif");
@@ -41,11 +42,10 @@ public class Anggota {
         }
     }
 
-    
     public int getJumlahPinjaman() {
         return jumlahPinjaman;
     }
-    
+
     public void pinjam(int jumlah) {
         if (jumlah <= 0) {
             System.out.println("Jumlah pinjaman harus lebih dari 0");
@@ -58,17 +58,17 @@ public class Anggota {
     }
 
     public void angsur(int jumlah) {
+        double minimalAngsuran = jumlahPinjaman * 0.1; // MODIFIKASI ANGSURAN MINIMAL 10% DARI JUMLAH PINJAMAN
         if (jumlah <= 0) {
             System.out.println("Jumlah angsuran harus lebih dari 0");
+        } else if (jumlah < minimalAngsuran) {
+            System.out.println("Maaf, jumlah angsuran harus minimal 10% dari jumlah pinjaman");
         } else if (jumlah > jumlahPinjaman) {
             System.out.println("Maaf, jumlah angsuran melebihi jumlah pinjaman");
         } else {
             jumlahPinjaman -= jumlah;
             System.out.println("Angsuran berhasil. Jumlah pinjaman saat ini: " + jumlahPinjaman);
-                }
+        }
     }
-    
 
-
-    
 }
